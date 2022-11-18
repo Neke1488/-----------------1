2008dbebd4a145be7f03d612b5ee07976662bc62
Nikita <neket32167@yandex.ru>
2022-11-18 21:39:31 +0300

    add docker with postgres

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 const express = require("express");
-const studentRouter = require('./routes/student.routes')
+const studentRouter = require('../routes/student.routes')
 
 const app = express();
 app.use(express.json())
 app.use('/api', studentRouter)
-
 app.get("/", function(request, response){
      
   response.send("Hello NPI!!!");
