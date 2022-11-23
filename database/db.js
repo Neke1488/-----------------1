a62b804ddcef022c24002a09fe9812b4410a295e
Nikita <neket32167@yandex.ru>
2022-11-23 23:36:37 +0300

    fixing

--- database/db.js
+++ database/db.js
@@ -1,8 +1,8 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
-    user: "postgres",
-    password: 'root',
-    host: "localhost",
-    port: 5432,
-    database: ""
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME
 })
